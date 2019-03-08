452c929e5bb87b6fc3bf1e1ef28a8fe443952af6
erwin-real <erwinsantoscapati@gmail.com>
2019-03-08 11:30:14 +0800

    Test Commit 5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    TEST
+                    TEST 4
                 </div>
 
                 <div class="links">
